addf6b06ff87f829f50dfa549ea96b60ec01e042
andygrun <andersongrun@hotmail.com>
2026-09-19 00:48:26 +0100

    Add Terraform SSO data source and outputs

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -9,6 +9,21 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-west-1"
+  region  = "eu-west-1"
   profile = "admin"
 }
+
+provider "aws" {
+
+  alias   = "sso"
+  region  = "eu-north-1"
+  profile = "admin"
+}
+
+data "aws_ssoadmin_instances" "this" {
+  provider = aws.sso
+}
+
+output "arn" { value = tolist(data.aws_ssoadmin_instances.this.arns)[0] }
+
+output "identity_store_id" { value = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0] }
